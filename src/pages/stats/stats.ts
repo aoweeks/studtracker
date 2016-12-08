a582462b3960bec1a216e5c0b0175f9da8d2be4f
aoweeks <aoweeks@gmail.com>
2016-12-08 16:30:32 +0000

    Change odds text classes if hand is full or not

--- src/pages/stats/stats.ts
+++ src/pages/stats/stats.ts
@@ -21,6 +21,7 @@ export class StatsPage {
 	private handRanks = ["PAIR", "2 PAIR", "3 OF A KIND", "STRAIGHT", "FLUSH",
 						 "FULL HOUSE", "4 OF A KIND", "STRAIGHT FLUSH"];
 	public handRows = [0,2,4,6];
+	private numberOfCardsRemaining;
 
   constructor(	public navCtrl: NavController,
   				private analyser: HandAnalyser,
@@ -28,10 +29,10 @@ export class StatsPage {
   				private settingsData: SettingsData) {
 
 
-    let numberOfCardsRemaining: number = this.settingsData.getFiveOrSevenMode() - this.playerHand.getHandSize();
+    this.numberOfCardsRemaining = this.settingsData.getFiveOrSevenMode() - this.playerHand.getHandSize();
     console.log("Made it into constructor");
-    if(numberOfCardsRemaining > 0){
-  		this.analyser.calculatePotentialHandValues(playerHand.getHand(), numberOfCardsRemaining);
+    if(this.numberOfCardsRemaining > 0){
+  		this.analyser.calculatePotentialHandValues(playerHand.getHand(), this.numberOfCardsRemaining);
   	} else{
   		this.analyser.calculateFullHandOdds(playerHand.getHand());
   	}
